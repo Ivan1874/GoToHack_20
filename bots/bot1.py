ef7fa182f71c35c32d4ecdeb9aa44d932ef589bc
allone00 <raldugin06@mail.ru>
2020-01-04 19:46:52 +0400

    #test bot

--- bots/bot1.py
+++ bots/bot1.py
@@ -1,36 +1,128 @@
-poligon = [['.'] * 30 for i in range(30)]
+poligon = [['.'] * 9 for i in range(9)]
 
 my_color = '*'
+flag = True; flagX = flagY = True
 
-moves = []
+coord_x = coord_y = 0
+
+def Tick(coord_x, coord_y):
+    return move()
+
+def move():
+    global poligon, my_color, flag, flagX, flagY, coord_x, coord_y
+    if flag:
+        if coord_x > 0 and my_color != get_pixel(coord_x - 1, coord_y) and flagX:
+            coord_x -= 1
+            return 'move_left()'
+        elif coord_x < len(poligon[coord_y]) - 1 and my_color != get_pixel(coord_x + 1, coord_y) and not flagX:
+            coord_x += 1
+            return 'move_right()'
+        elif coord_y > 0 and my_color != get_pixel(coord_x, coord_y - 1) and flagY:
+            coord_y -= 1
+            return 'move_up()'
+        elif coord_y < len(poligon) - 1 and my_color != get_pixel(coord_x, coord_y + 1) and not flagY:
+            coord_y += 1
+            return 'move_down()'
+        if coord_x == len(poligon[coord_y]) - 1:
+            flagX = True
+        if coord_x == 0:
+            flagX = False
+        if coord_y == len(poligon) - 1:
+            flagY = True
+        if coord_y == 0:
+            flagY = False
+
+    if coord_x < len(poligon[coord_y]) - 1 and my_color == get_pixel(coord_x + 1, coord_y):
+        flag = False
+        relaxe()
+    elif coord_x > 0 and my_color == get_pixel(coord_x - 1, coord_y):
+        flag = False
+        relaxe()
+    elif coord_y > 0 and my_color == get_pixel(coord_x, coord_y - 1):
+        flag = False
+        relaxe()
+    elif coord_y < len(poligon) and my_color == get_pixel(coord_x, coord_y  + 1):
+        flag = False
+        relaxe()
+
+def relaxe():
+    global coord_y, coord_x, flag
+    if check_better(coord_x, coord_y) == 'go_left()':
+        while coord_x > 0 and my_color == get_pixel(coord_x, coord_y):
+            coord_x -= 1
+            return 'move_left()'
+    elif check_better(coord_x, coord_y) == 'go_right()':
+        while coord_x < len(poligon[coord_y]) and my_color == get_pixel(coord_x, coord_y):
+            coord_x += 1
+            return 'move_right()'
+    elif check_better(coord_x, coord_y) == 'go_up()':
+        while coord_y > 0 and my_color == get_pixel(coord_x, coord_y):
+            coord_y -= 1
+            return 'move_up()'
+    elif check_better(coord_x, coord_y) == 'go_down()':
+        while coord_y < len(poligon) and my_color == get_pixel(coord_x, coord_y):
+            coord_y += 1
+            return 'move_down()'
+    if coord_x < len(poligon[coord_y]) - 1 and my_color != get_pixel(coord_x + 1, coord_y):
+        flag = True
+    elif coord_x > 0 and my_color != get_pixel(coord_x - 1, coord_y):
+        flag = True
+    elif coord_y < len(poligon) and my_color != get_pixel(coord_x, coord_y):
+        flag = True
+    elif coord_y > 0 and my_color != get_pixel(coord_x, coord_y):
+        flag = True
+    else:
+        if 0 < coord_x:
+            coord_x -= 1
+            move()
+        elif coord_x < len(poligon[coord_y]) - 1:
+            coord_x += 1
+            move()
+        elif coord_y > 0:
+            coord_y -= 1
+            move()
+        elif coord_y < len(poligon) - 1:
+            coord_y += 1
+            move()
 
 
-def position(move, coord_x, coord_y):
-    global moves
-    moves.append((move, coord_x, coord_y))
 
 
-def bot(coord_x, coord_y, color):
+def check_better(coord_x, coord_y):
+    Lcoord_x = Dcoord_y = Rcoord_x = Ucoord_y = 0
+    old_coords = (coord_x, coord_y)
+    while coord_x > 0 and get_pixel(coord_x, coord_y) == my_color:
+        coord_x -= 1; Lcoord_x += 1
+    coord_x = old_coords[0]
+    while coord_x < len(poligon[coord_y]) and get_pixel(coord_x, coord_y) == my_color:
+        coord_x += 1; Rcoord_x += 1
+    coord_x = old_coords[0]
+    while coord_y > 0 and get_pixel(coord_x, coord_y):
+        coord_y -= 1; Ucoord_y += 1
+    coord_y = old_coords[1]
+    while coord_y < len(poligon) and get_pixel(coord_x, coord_y):
+        coord_y += 1; Dcoord_y += 1
+    coord_y = old_coords[1]
+    print(Lcoord_x, Rcoord_x, Ucoord_y, Dcoord_y, 'its coords', coord_y, coord_x)
+    if min(Lcoord_x, Rcoord_x, Ucoord_y, Dcoord_y) == Lcoord_x:
+        return 'go_left()'
+    elif min(Lcoord_x, Rcoord_x, Ucoord_y, Dcoord_y) == Rcoord_x:
+        return 'go_right()'
+    elif min(Lcoord_x, Rcoord_x, Ucoord_y, Dcoord_y) == Ucoord_y:
+        return 'go_up()'
+    elif min(Lcoord_x, Rcoord_x, Ucoord_y, Dcoord_y) == Dcoord_y:
+        return 'go_down()'
+
+
+def get_pixel(coord_x, coord_y):
     global poligon
-    if coord_x < 0 or coord_x > len(poligon[coord_y]) or coord_y < 0 or coord_y > len(poligon) or color == my_color:
-        return
+    return poligon[coord_y][coord_x]
+
+i = 0
+
+while i < 90:
+    Tick(coord_x, coord_y)
     poligon[coord_y][coord_x] = '*'
-    if coord_x > 0 and color != my_color:
-        position('coord_x - 1', coord_x - 1, coord_y)
-        bot(coord_x - 1, coord_y, poligon[coord_y][coord_x - 1])
-    if coord_x < len(poligon[coord_y]) - 1 and color != my_color:
-        position('coord_x + 1', coord_x + 1, coord_y)
-        bot(coord_x + 1, coord_y, poligon[coord_y][coord_x + 1])
-    if coord_y > 0 and color != my_color:
-        position('coord_y - 1', coord_x, coord_y - 1)
-        bot(coord_x, coord_y - 1, poligon[coord_y - 1][coord_x])
-    if coord_y < len(poligon) - 1 and color != my_color:
-        position('coord_y + 1', coord_x, coord_y + 1)
-        bot(coord_x, coord_y + 1, poligon[coord_y + 1][coord_x])
+    i += 1
 
-
-bot(0, 0, '.')
-
-# print(*poligon, sep='\n')
-
-print(moves[:20])
+print(*poligon, sep='\n')
--- bots/bot2.py
+++ bots/bot2.py
@@ -1,22 +1,45 @@
-poligon = [['.'] * 30 for i in range(30)]
+from random import randint
+
+poligon = [['.'] * 10 for i in range(10)]
 
 my_color = '*'
 
-coord_x = coord_y = 0
+coord_x = coord_y = 0;
+flagX = flagY = False
+
 
 def Tick(coord_x, coord_y):
-    pixel = get_pixel(coord_x, coord_y)
+    return move(randint(1, 2))
 
-def move(coord_x, coord_y):
-    global poligon
-    if coord_x > 0 and get_pixel(coord_x, coord_y) != my_color:
-        return 'move_left()'
-    elif coord_x < len(poligon[coord_y]) - 1 and get_pixel(coord_x, coord_y) != my_color:
-        return 'move_right()'
-    elif coord_y > 0 and get_pixel(coord_x, coord_y) != my_color:
-        return 'move_down()'
-    elif coord_y < len(poligon) - 1 and get_pixel(coord_x, coord_y) != my_color:
-        return 'move_up()'
+
+def move(x):
+    global poligon, coord_x, coord_y, flagX, flagY, my_color
+    if x == 1:
+        if coord_x > 0 and get_pixel(coord_x - 1, coord_y) != my_color and flagX and (0 <= coord_y <= len(poligon) - 1):
+            coord_x -= 1
+            return 'move_left()'
+        elif coord_x < len(poligon[coord_y]) - 1 and get_pixel(coord_x + 1, coord_y) != my_color and not flagX and (
+                0 <= coord_y <= len(poligon) - 1):
+            coord_x += 1
+            return 'move_right()'
+    elif x == 2:
+        if coord_y > 0 and get_pixel(coord_x, coord_y - 1) != my_color and (
+                0 <= coord_x <= len(poligon[coord_y]) - 1) and flagY:
+            coord_y -= 1
+            return 'move_up()'
+        elif coord_y < len(poligon) - 1 and get_pixel(coord_x, coord_y + 1) != my_color and (
+                0 <= coord_x <= len(poligon[coord_y]) - 1) and not flagY:
+            coord_y += 1
+            return 'move_down()'
+
+    if coord_x == len(poligon[coord_y]) - 1:
+        flagX = True
+    if coord_x == 0:
+        flagX = False
+    if coord_y == len(poligon) - 1:
+        flagY = True
+    if coord_y == 0:
+        flagY = False
 
 
 def get_pixel(coord_x, coord_y):
@@ -24,4 +47,9 @@ def get_pixel(coord_x, coord_y):
     return poligon[coord_y][coord_x]
 
 
-while True:
+i = 0
+while i < 60:
+    Tick(coord_x, coord_y)
+    poligon[coord_y][coord_x] = '*'
+    i += 1
+print(*poligon, sep='\n')
